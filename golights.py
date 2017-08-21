28841d86dca6fcff00db78963fa958a1efaa62ef
Doug Gibson <dug.gibson@gmail.com>
2017-08-21 22:15:01 +0800

    added program code

---
+++ golights.py
@@ -0,0 +1,21 @@
+from gpiozero import LED
+from time import sleep
+import sys
+
+redLightPin = 14
+yellowLightPin = 15
+greenLightPin = 18
+
+redLED = LED(redLightPin)
+yellowLED = LED(yellowLightPin)
+greenLED = LED(greenLightPin)
+
+greenLED.on()
+sleep(1)
+greenLED.off()
+yellowLED.on()
+sleep(1)
+yellowLED.off()
+redLED.on()
+sleep(1)
+redLED.off()
